72a2e9ea26387bdcbab34e933ae7abd2f0a89da8
Joshua B Gutierrez <joshgm15@gmail.com>
2020-10-07 21:33:32 -0500

    Added animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -39,3 +39,42 @@ menuItems.forEach((item) => {
 		isDisplayed = !isDisplayed;
 	});
 });
+
+// Animations
+const showcaseSection = document.querySelector('section.showcase');
+const revealer = document.querySelector('.revealer');
+const aboutSection = document.querySelector('section.about-section');
+const greeting = document.querySelector('.about-section header h3');
+const aboutMe = document.querySelector('.about-section article p');
+const skillsSection = document.querySelector('.skills-section-content');
+const oddSkillsContainers = document.querySelectorAll('.skills:nth-child(odd)');
+const evenSkillsContainers = document.querySelectorAll('.skills:nth-child(even)');
+
+const tl = gsap.timeline({
+	defaults: { ease: 'power2.out' }
+});
+
+const tl2 = gsap.timeline({
+	scrollTrigger: {
+		trigger: aboutSection
+	}
+});
+
+const tl3 = gsap.timeline({
+	scrollTrigger: {
+		trigger: skillsSection
+	}
+});
+
+// SHOWCASE ANIMATIONS
+tl.fromTo(revealer, { scale: 0, opacity: 1 }, { scale: 300, opacity: 1, duration: 1 });
+tl.to(revealer, { opacity: 0, display: 'none', duration: 1 });
+tl.from(mainHeader, { y: -100, duration: 1 }, '-=1');
+tl.from(showcaseSection, { x: -2000, duration: 1 }, '-=1');
+
+// ABOUT SECTION ANIMATIONS
+tl2.fromTo([ greeting, aboutMe ], { opacity: 0, y: 50, delay: 0.5 }, { opacity: 1, duration: 1, y: 0 });
+
+// SKILLS ANIMATIONS
+tl3.from(oddSkillsContainers, { x: -1000, duration: 1 }, '-=1');
+tl3.from(evenSkillsContainers, { x: 1000, duration: 1 }, '-=1');
